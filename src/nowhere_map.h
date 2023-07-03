eb7c847c9311ff3903935605334cf3502c45acc3
mynahisnowhere <137255931+mynahisnowhere@users.noreply.github.com>
2023-07-03 10:04:32 -0700

    Removed unused function. nowhere_free is the appropriate function

--- src/nowhere_map.h
+++ src/nowhere_map.h
@@ -1,6 +1,8 @@
 #ifndef NOWHERE_MAP_H
 #define NOWHERE_MAP_H
 
+#include <stddef.h>
+
 typedef struct nowhere_map *nowhere_map_t;
 
 int nowhere_map_create(nowhere_map_t *_map, size_t _key_count, size_t _entry_size);
@@ -8,7 +10,5 @@ int nowhere_map_create(nowhere_map_t *_map, size_t _key_count, size_t _entry_siz
 void nowhere_map_put(nowhere_map_t _map, void *_key, const void *_data);
 
 void *nowhere_map_get(nowhere_map_t _map, const void *_key);
-
-void nowhere_map_destroy(nowhere_map_t _map);
 
 #endif //NOWHERE_MAP_H
