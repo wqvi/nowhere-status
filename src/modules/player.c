5c957e743b866857c5fbd7413cebe902e5d6d5e3
mynahisnowhere <137255931+mynahisnowhere@users.noreply.github.com>
2023-10-23 16:08:12 -0700

    Fixed application from constantly instantiating playerctl objects.

--- src/modules/player.c
+++ src/modules/player.c
@@ -2,12 +2,18 @@
 #include "playerctl/playerctl.h"
 #include <stdio.h>
 
+static PlayerctlPlayer *player = NULL;
+
 int nowhere_player(struct node *_node) {
-	PlayerctlPlayer *player = playerctl_player_new(NULL, NULL);
+	if (!player) {
+		player = playerctl_player_new(NULL, NULL);
+	}
 
 	gchar *artist = playerctl_player_get_artist(player, NULL);
 	gchar *title = playerctl_player_get_title(player, NULL);
 
 	snprintf(_node->name, NOWHERE_NAMSIZ, "player");
 	snprintf(_node->full_text, NOWHERE_TXTSIZ, "%s by %s", title, artist);
+
+	return 0;
 }
