633b7fb1baa00509d71feb8ec9149b058af7981b
mynahisnowhere <137255931+mynahisnowhere@users.noreply.github.com>
2023-10-24 08:50:38 -0700

    Apparently GObjects are reference based. This means the prior commit is useless.

--- src/modules/player.c
+++ src/modules/player.c
@@ -2,12 +2,8 @@
 #include "playerctl/playerctl.h"
 #include <stdio.h>
 
-static PlayerctlPlayer *player = NULL;
-
 int nowhere_player(struct node *_node) {
-	if (!player) {
-		player = playerctl_player_new(NULL, NULL);
-	}
+	PlayerctlPlayer *player = playerctl_player_new(NULL, NULL);
 
 	gchar *artist = playerctl_player_get_artist(player, NULL);
 	gchar *title = playerctl_player_get_title(player, NULL);
