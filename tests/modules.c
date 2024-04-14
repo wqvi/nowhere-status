f6e4847fc5af86ad97db5437ffa0f8479f7e2e17
wqvi <137255931+wqvi@users.noreply.github.com>
2024-04-13 17:45:07 -0700

    Rewrote tests file; readable and loops through the test cases.

--- tests/modules.c
+++ tests/modules.c
@@ -13,152 +13,70 @@ int sanitize(char *_str, const char *_initial_str, size_t _initial_length);
 void trim_whitespace(char *_str, size_t _len);
 
 START_TEST(test_playerctl_sanitize_function) {
-#define PHRASE "9\'99\" \'TTTT TTT EEE"
-#define EXPECTED "9\'99\\\" \'T..."
+	const char *phrases[] = {
+		"aaaaaaaaa",
+		"9\'99\" \'TTTT TTT EEE",
+		"EEEEEEEEEEEEEEE",
+		"RRRRRRRR AAAAA (RRRRR EEEE)",
+		"RRRRRRRR AAAA (RRRRR EEEE)",
+		"DDDDDDDDDDDDDDD (MMMMMM RRRRR)",
+		"DDDDDDDDDDDDDDDD (MMMMMM RRRRR)",
+	};
+	const char *expects[] = {
+		"aaaaaaaaa",
+		"9\'99\\\" \'T...",
+		"EEEEEEEEEEEEEEE",
+		"RRRRRRRR AAAAA",
+		"RRRRRRRR AAAA",
+		"DDDDDDDDDDDDDDD",
+		"DDDDDDDDDDDDDDDD",
+	};
 
-	char actual[32];
-	memset(actual, 0, sizeof(actual));
-	memcpy(actual, PHRASE, 16);
-	sanitize(actual, PHRASE, strlen(PHRASE));
+	char actual[64];
 
-	ck_assert_str_eq(actual, EXPECTED);
+	for (int i = 0; i < sizeof(phrases) / sizeof(const char *); i++) {
+		const char *phrase = phrases[i];
+		const char *expected = expects[i];
 
-	ck_assert_int_eq(strlen(actual), strlen(EXPECTED));
+		memset(actual, 0, sizeof(actual));
+		memcpy(actual, phrase, strlen(phrase));
+		actual[16] = '\0';
+		sanitize(actual, phrase, strlen(phrase));
 
-#undef PHRASE
-#define PHRASE "EEEEEEEEEEEEEEE"
-#undef EXPECTED
-#define EXPECTED "EEEEEEEEEEEEEEE"
-
-	memset(actual, 0, sizeof(actual));
-	memcpy(actual, PHRASE, 16);
-	sanitize(actual, PHRASE, strlen(PHRASE));
-
-	ck_assert_str_eq(actual, EXPECTED);
-
-	ck_assert_int_eq(strlen(actual), strlen(EXPECTED));
-
-#undef PHRASE
-#define PHRASE "RRRRRRRR AAAAA (RRRRR EEEE)"
-#undef EXPECTED
-#define EXPECTED "RRRRRRRR AAAAA"
-
-	memset(actual, 0, sizeof(actual));
-	memcpy(actual, PHRASE, 16);
-	sanitize(actual, PHRASE, strlen(PHRASE));
-
-	ck_assert_str_eq(actual, EXPECTED);
-
-	ck_assert_int_eq(strlen(actual), strlen(EXPECTED));
-
-#undef PHRASE
-#define PHRASE "DDDDDDDDDD 9.9 (MMMMMM RRRRR)"
-#undef EXPECTED
-#define EXPECTED "DDDDDDDDDD 9.9"
-
-	memset(actual, 0, sizeof(actual));
-	memcpy(actual, PHRASE, 16);
-	sanitize(actual, PHRASE, strlen(PHRASE));
-
-	ck_assert_str_eq(actual, EXPECTED);
-
-	ck_assert_int_eq(strlen(actual), strlen(EXPECTED));
-
-#undef PHRASE
-#define PHRASE "AA AAAA A'A AAAAAAA AA"
-#undef EXPECTED
-#define EXPECTED "AA AAAA A'A A..."
-
-	memset(actual, 0, sizeof(actual));
-	memcpy(actual, PHRASE, 16);
-	sanitize(actual, PHRASE, strlen(PHRASE));
-
-	ck_assert_str_eq(actual, EXPECTED);
-
-	ck_assert_int_eq(strlen(actual), strlen(EXPECTED));
-
-#undef PHRASE
-#define PHRASE "aaaa bbbbbbbb bbbbbb on X: \"aaa bb ccc ddddddd eeeeeee ffff ggggggggg hhhh i jjjjjj kkkkk lll mnopq://r.st/uvwxyzzzzx\" / X"
-#undef EXPECTED
-#define EXPECTED 'X'
-
-	memset(actual, 0, sizeof(actual));
-	memcpy(actual, PHRASE, 16);
-	(*(int *)&actual) = sanitize(actual, PHRASE, strlen(PHRASE));
-
-	ck_assert_int_eq((*(int *)&(actual)), EXPECTED);
+		ck_assert_str_eq(actual, expected);
+	}
 }
 END_TEST
 
 START_TEST(test_playerctl_sstrr_function) {
-#undef PHRASE
-#define PHRASE "9\'99\" \'TTTT TTT EEE"
-#undef EXPECTED
-#define EXPECTED "99\'99\" \'TTTT TTT EEE"
+	const char *phrases[] = {
+		"9\'99\\\" \'T",
+		"abc",
+		"(())***",
+	};
+	const char *expects[] = {
+		"99\'99\\\" \'T",
+		"aabc",
+		"((())***",
+	};
 
-	char actual[32] = PHRASE;
+	char actual[64];
 
-	sstrr(actual, strlen(actual) + 1);
+	for (int i = 0; i < sizeof(phrases) / sizeof(const char *); i++) {
+		const char *phrase = phrases[i];
+		const char *expected = expects[i];
 
-	ck_assert_str_eq(actual, EXPECTED);
+		memset(actual, 0, sizeof(actual));
+		memcpy(actual, phrase, strlen(phrase));
+		actual[16] = '\0';
+		sstrr(actual, strlen(actual) + 1);
 
-#undef PHRASE
-#define PHRASE "abc"
-#undef EXPECTED
-#define EXPECTED "aabc"
-
-	memset(actual, 0, sizeof(actual));
-	snprintf(actual, 4, PHRASE);
-
-	sstrr(actual, strlen(actual) + 1);
-
-	ck_assert_str_eq(actual, EXPECTED);
-
-#undef PHRASE
-#define PHRASE "(())***"
-#undef EXPECTED
-#define EXPECTED "((())***"
-
-	memset(actual, 0, sizeof(actual));
-	snprintf(actual, 8, PHRASE);
-
-	sstrr(actual, strlen(actual) + 1);
-
-	ck_assert_str_eq(actual, EXPECTED);
-}
-END_TEST
-
-START_TEST(test_playerctl_sstrl_function) {
-#undef PHRASE
-#define PHRASE "abc_abbbc"
-#undef EXPECTED
-#define EXPECTED "abc_bbbc"
-
-	char actual[32] = PHRASE;
-
-	memset(actual, 0, sizeof(actual));
-	snprintf(actual, sizeof(PHRASE), PHRASE);
-
-	sstrl(actual + 4, strlen(actual) - 4);
-
-	ck_assert_str_eq(actual, EXPECTED);
-
-#undef PHRASE
-#define PHRASE "(())***"
-#undef EXPECTED
-#define EXPECTED "())***"
-
-	memset(actual, 0, sizeof(actual));
-	snprintf(actual, 8, PHRASE);
-
-	sstrl(actual, strlen(actual));
-
-	ck_assert_str_eq(actual, EXPECTED);
+		ck_assert_str_eq(actual, expected);
+	}
 }
 END_TEST
 
 START_TEST(test_trim_whitespace_function) {
-#undef PHRASE
 #define PHRASE "123456789012345 12345"
 #undef EXPECTED
 #define EXPECTED "123456789012345"
@@ -183,16 +101,13 @@ Suite *modules_suite(void) {
 	s = suite_create("modules");
 
 	tc_san = tcase_create("sanitize");
-	tc_str = tcase_create("strings");
+	tc_str = tcase_create("shift string right");
 	tc_trim = tcase_create("trim");
 
 	tcase_add_test(tc_san, test_playerctl_sanitize_function);
 	suite_add_tcase(s, tc_san);
 
 	tcase_add_test(tc_str, test_playerctl_sstrr_function);
-	suite_add_tcase(s, tc_str);
-
-	tcase_add_test(tc_str, test_playerctl_sstrl_function);
 	suite_add_tcase(s, tc_str);
 
 	tcase_add_test(tc_trim, test_trim_whitespace_function);
