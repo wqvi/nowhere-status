730dffba1a43f9657d17021b633e941d2c5240eb
wqvi <137255931+wqvi@users.noreply.github.com>
2024-04-19 08:47:12 -0700

    Added test case for getting node. Uses check add_loop_test now.
    All looped tests need to be changed to use add_loop_test from check
    library.

--- tests/llist.c
+++ tests/llist.c
@@ -2,66 +2,89 @@
 #include <swaybar.h>
 #include <stdlib.h>
 
+static struct node *heads[2] = {
+	NULL
+};
+
 START_TEST(test_linked_list_create_function) {
-	struct node *heads[] = {
-		NULL
-	};
-	struct node_info infos[2][2] = {
+	static struct node_info infos[2][4] = {
 		{
 			{
 				NOWHERE_NODE_DEFAULT,
-				'T',
+				'A',
 				NULL
 			},
 			{
 				NOWHERE_NODE_DEFAULT,
-				'T',
+				'B',
 				NULL
 			}
 		},
 		{
 			{
 				NOWHERE_NODE_DEFAULT,
-				'T',
+				'C',
 				NULL,
 			},
 			{
 				NOWHERE_NODE_DEFAULT,
-				'T',
+				'D',
 				NULL
 			}
 		}
 	};
-	int expects[] = {
+	static int expects[] = {
+		0,
 		0
 	};
 
-	for (int i = 0; i < sizeof(heads) / sizeof(struct node *); i++) {
-		struct node *head = heads[i];
-		struct node_info *info = infos[i];
-		int expected = expects[i];
+	struct node *head = heads[_i];
+	struct node_info *info = infos[_i];
+	int expected = expects[_i];
 
-		int ret = llist_create(&head, info, 2);
+	int ret = llist_create(&head, info, 2);
 
-		ck_assert_int_eq(ret, expected);
+	ck_assert_int_eq(ret, expected);
 
-		ck_assert_ptr_nonnull(head);
+	ck_assert_ptr_nonnull(head);
+}
+END_TEST
 
-		free(head);
-	}
+START_TEST(test_linked_list_get_function) {
+	static char names[] = {
+		'A',
+		'B',
+		'C',
+		'D'
+	};
+
+       struct node *head = heads[_i];
+       char name = names[_i];
+
+       struct node *node = llist_get(head, name);
+
+       //ck_assert_ptr_nonnull(node);
+
+       free(head);
+
 }
 END_TEST
 
 Suite *linked_list_suite(void) {
 	Suite *s;
 	TCase *tc_create;
+	TCase *tc_free;
 
 	s = suite_create("linked_list");
 
 	tc_create = tcase_create("linked list create");
+	tc_free = tcase_create("linked list get");
 
-	tcase_add_test(tc_create, test_linked_list_create_function);
+	tcase_add_loop_test(tc_create, test_linked_list_create_function, 0, 2);
 	suite_add_tcase(s, tc_create);
+
+	tcase_add_loop_test(tc_free, test_linked_list_get_function);
+	suite_add_tcase(s, tc_free);
 
 	return s;
 }
